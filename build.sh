5878bd768ee7f250f3bd574733e9b0bc09ed8e4f
Divyanshu <divyanshussa@gmail.com>
2025-08-17 15:48:55 +0530

    fix production bugs

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
-echo "Installing dependencies..."
+#!/bin/bash
+echo "----- Forcing Python 3.10 -----"
+pyenv install 3.10.13 -s
+pyenv global 3.10.13
+python --version
+
+echo "----- Installing dependencies -----"
+pip install --upgrade pip
 pip install -r requirements.txt
-echo "Build completed - using model files from repository"
