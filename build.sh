9802478cfc1d2c0861bb67b66f58ae815a745ce8
Divyanshu <divyanshussa@gmail.com>
2025-08-17 23:38:05 +0530

    remove production file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "----- Forcing Python 3.10 -----"
-pyenv install 3.10.13 -s
-pyenv global 3.10.13
-python --version
-
-echo "----- Installing dependencies -----"
-pip install --upgrade pip
-pip install -r requirements.txt
